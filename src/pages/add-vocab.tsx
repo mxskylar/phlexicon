2dec56a644830759066ce458d1089c50d8a1cd56
Skylar Pape <11418651+skylarpape@users.noreply.github.com>
2025-03-24 16:51:38 -0400

    Renames DB

--- src/pages/add-vocab.tsx
+++ src/pages/add-vocab.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 
 export const AddVocab = () => {
     const add = () => {
-        //query("CREATE DATABASE asl;");
+        //query("CREATE DATABASE phlexicon;");
         console.log("hi")
     }
     return (
