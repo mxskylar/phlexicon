309ca921d6d6990b99495c236960cbbeb4f2553f
Skylar Pape <11418651+skylarpape@users.noreply.github.com>
2025-03-25 17:18:13 -0400

    Adds URLs to comments

--- db/install-db-data.js
+++ db/install-db-data.js
@@ -14,8 +14,11 @@ const downloadFile = async (url, fileName) => {
 };
 
 // ISO language codes & names
+// See Language Names Index at: https://iso639-3.sil.org/code_tables/download_tables
 await downloadFile("https://iso639-3.sil.org/sites/iso639-3/files/downloads/iso-639-3_Name_Index.tab", "iso-languages.tab");
 
 // Phoible data on spoken languages
+// https://phoible.org/download
+// https://github.com/phoible/dev/tree/master/data
 await downloadFile("https://raw.githubusercontent.com/phoible/dev/master/mappings/InventoryID-LanguageCodes.csv", "spoken-languages.csv");
 await downloadFile("https://raw.githubusercontent.com/phoible/dev/master/data/phoible.csv", "spoken-phonemes.csv");
