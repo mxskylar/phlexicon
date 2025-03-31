5fd0d452c433010d3745db739d51dac88866a848
Skylar Pape <11418651+skylarpape@users.noreply.github.com>
2025-03-31 13:02:51 -0400

    Reads text input again

--- install-utils.js
+++ install-utils.js
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import {mkdir} from 'fs/promises';
+import {Readable} from 'stream';
 
 export const downloadFile = async (url, filePath, fileName) => {
     console.log(`Downloading ${filePath}/${fileName} from: ${url}`)
@@ -8,7 +9,8 @@ export const downloadFile = async (url, filePath, fileName) => {
         await mkdir(filePath);
     }
     const fileStream = fs.createWriteStream(`${filePath}/${fileName}`);
+    Readable.fromWeb(response.body).pipe(fileStream);
     fileStream.on("finish", () => {
-        file.close();
+        fileStream.close();
     });
 };
