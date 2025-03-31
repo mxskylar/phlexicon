b54a61d8feadfccda5000d851b6011f2ae4088df
Skylar Pape <11418651+skylarpape@users.noreply.github.com>
2025-03-31 01:43:51 -0400

    Creates union view

--- db/etl.sql
+++ db/etl.sql
@@ -15,3 +15,11 @@ FROM (
 
 ALTER TABLE spoken_phonemes DROP COLUMN language_variety;
 ALTER TABLE spoken_phonemes DROP COLUMN dialect_description;
+
+-- Create union views of spoken & sign language tables
+CREATE VIEW dialects AS
+SELECT *, "SPOKEN" AS language_type
+FROM spoken_dialects
+UNION
+SELECT *, "SIGN" AS language_type
+FROM sign_dialects;
