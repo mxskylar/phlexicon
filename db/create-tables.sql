fefff8a983b9bb78b56f1c4369d69d1233a143df
Skylar Pape <11418651+skylarpape@users.noreply.github.com>
2025-03-30 21:50:23 -0400

    Capitalizes query keywords

--- db/create-tables.sql
+++ db/create-tables.sql
@@ -1,22 +1,22 @@
 
 DROP TABLE IF EXISTS main.iso_languages;
 CREATE TABLE IF NOT EXISTS main.iso_languages(
-    id char(3) NOT NULL,
-    print_name varchar(75) NOT NULL,
-    inverted_name varchar(75) NOT NULL
+    id CHAR(3) NOT NULL,
+    print_name VARCHAR(75) NOT NULL,
+    inverted_name VARCHAR(75) NOT NULL
 );
 
 DROP TABLE IF EXISTS main.spoken_languages;
 CREATE TABLE IF NOT EXISTS main.spoken_languages(
-    language_variety string NOT NULL PRIMARY KEY,
-    iso_code char(3) NOT NULL,
+    language_variety STRING NOT NULL PRIMARY KEY,
+    iso_code CHAR(3) NOT NULL,
     FOREIGN KEY(iso_code) REFERENCES iso_languages(id)
 );
 
 DROP TABLE IF EXISTS main.sign_languages;
 CREATE TABLE IF NOT EXISTS main.sign_languages(
-    sign_puddle_dictionary string NOT NULL PRIMARY KEY,
-    region string NOT NULL,
-    iso_code char(3) NOT NULL,
+    sign_puddle_dictionary STRING NOT NULL PRIMARY KEY,
+    region STRING NOT NULL,
+    iso_code CHAR(3) NOT NULL,
     FOREIGN KEY(iso_code) REFERENCES iso_languages(id)
 );
