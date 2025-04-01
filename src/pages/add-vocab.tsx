2963782756c96c1bccf7e152ea6ca26cb1cab7d5
Skylar Pape <11418651+skylarpape@users.noreply.github.com>
2025-04-01 16:17:21 -0400

    Removes anki client

--- src/pages/add-vocab.tsx
+++ src/pages/add-vocab.tsx
@@ -1,18 +1,11 @@
 import * as React from 'react';
 import {Keyboard} from '../components/keyboard'
-import {ankiPost} from '../utils/anki-client'
 
 export const AddVocab = () => {
-    ankiPost({action: "deckNames"})
-        .then(result => console.log(result))
-        .catch(failureCallback => console.log(failureCallback));
-    const add = () => {
-        console.log("My query");
-    };
     return (
         <div className="container-fluid">
             <Keyboard/>
-            <button type="button" className="btn btn-primary" onClick={add}>Add</button>
+            <button type="button" className="btn btn-primary">Add</button>
         </div>
     );
 };
