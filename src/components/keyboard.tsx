b74a92404166cac2270e2c7cd8f17f1119124bf0
Skylar Pape <11418651+skylarpape@users.noreply.github.com>
2025-04-28 14:17:44 -0400

    Adds modals for phoneme symbols

--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -6,19 +6,42 @@ type Props = {
 };
 
 export const Keyboard = (props: Props) => {
+
     return (
         <div className="keyboard">
             {
-                props.keys.map((key, i) =>
+                props.keys.map((symbol, i) => (
                     <button
                         key={`key-${i}`}
                         type="button"
-                        className="btn btn-outline-secondary"
+                        className="btn btn-outline-secondary phoneme-symbol"
                         onClick={e => props.handleClick(e)}
+                        data-bs-toggle="modal"
+                        data-bs-target={`#${symbol}`}
                     >
-                        {key}
+                        {symbol}
                     </button>
-                )
+                ))
+            }
+            {
+                props.keys.map(symbol => (
+                    <div className="modal fade" id={symbol} tabIndex={-1} aria-hidden="true">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <div className="modal-title">
+                                        <h1 className="phoneme-symbol">{symbol}</h1>
+                                        <h6 className="text-secondary">Vowel</h6>
+                                    </div>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div className="modal-body">
+                                    <p>Body of modal</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))
             }
         </div>
     );
