31bf2007c24fa630fb51e00ee08ea8fafc2c6667
Skylar Pape <11418651+skylarpape@users.noreply.github.com>
2025-04-28 00:48:07 -0400

    Revert "Adds space for keyboard details"
    This reverts commit 2126d212f606e4ae5a6bf800268e92c09dc33dea.

--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -7,22 +7,19 @@ type Props = {
 
 export const Keyboard = (props: Props) => {
     return (
-        <div className="keyboard-container">
-            <div className="keyboard">
-                {
-                    props.keys.map((key, i) =>
-                        <button
-                            key={`key-${i}`}
-                            type="button"
-                            className="btn btn-outline-secondary"
-                            onClick={e => props.handleClick(e)}
-                        >
-                            {key}
-                        </button>
-                    )
-                }
-            </div>
-            <div className="key-details"></div>
+        <div className="keyboard">
+            {
+                props.keys.map((key, i) =>
+                    <button
+                        key={`key-${i}`}
+                        type="button"
+                        className="btn btn-outline-secondary"
+                        onClick={e => props.handleClick(e)}
+                    >
+                        {key}
+                    </button>
+                )
+            }
         </div>
     );
 }
