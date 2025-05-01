6219481d446aea2057624fbd464496e44eabd2d6
Skylar Pape <11418651+skylarpape@users.noreply.github.com>
2025-04-30 22:19:19 -0400

    Attempts to set palm symbols dynamically

--- src/pages/sign-languages.tsx
+++ src/pages/sign-languages.tsx
@@ -4,13 +4,18 @@ import { KEYBOARD_CONTROL_CLASS, PHONEME_SYMOL_CLASS } from '../constants.ts';
 import { sendQuery } from '../db/ipc.ts';
 import { SignDialect } from '../phonemes/sign/sign-dialect.ts';
 import { Hand } from '../phonemes/sign/hand.ts';
-import { Toolbar, ToolbarType } from '../components/toolbar.tsx';
+import { Toolbar, ToolbarButton, ToolbarType } from '../components/toolbar.tsx';
+import { CLOCKWISE_SIGN_WRITING_SYMBOL_ROTATIONS, SignWritingSymbolRotation } from '../phonemes/sign/sign-writing.ts';
 
 type Props = {};
 
 type State = {
     dialectOptions: Option[],
     hands: Hand[],
+    palmFilterHands: Hand[],
+    symbolRotationIndex: number,
+    symbolRotation: SignWritingSymbolRotation,
+    isRightHanded: boolean,
 };
 
 const ALL_LANGUAGES_VALUE = "ALL";
@@ -21,6 +26,10 @@ export class SignLanguages extends React.Component<Props, State> {
         this.state = {
             dialectOptions: [],
             hands: [],
+            palmFilterHands: [],
+            symbolRotationIndex: 0,
+            symbolRotation: SignWritingSymbolRotation.DEGREES_0_or_360,
+            isRightHanded: true,
         };
     }
 
@@ -44,15 +53,17 @@ export class SignLanguages extends React.Component<Props, State> {
         const dialects = await sendQuery("SELECT * FROM sign_dialects ORDER BY name;")
             .then(rows => rows as SignDialect[]);
         this.setState({
-            hands: await this.getHands(ALL_LANGUAGES_VALUE),
             dialectOptions: dialects.map(dialect => {
                 return {
                     displayText: dialect.name,
                     value: `${dialect.iso_code}-${dialect.region}`,
                 }
             }),
+            hands: await this.getHands(ALL_LANGUAGES_VALUE),
+            palmFilterHands: await sendQuery(
+                'SELECT * FROM hands WHERE base_symbol = "񆄱"'
+            ) as Hand[],
         });
-        console.log(this.state.hands);
     }
 
     async switchDialect(e: React.BaseSyntheticEvent<HTMLSelectElement>) {
@@ -61,10 +72,90 @@ export class SignLanguages extends React.Component<Props, State> {
         this.setState({
             hands: await this.getHands(dialectId),
         });
-        console.log(this.state.hands);
+    }
+
+    rotateClockwise() {
+        const incrementedIndex = this.state.symbolRotationIndex + 1;
+        const symbolRotationIndex = incrementedIndex > CLOCKWISE_SIGN_WRITING_SYMBOL_ROTATIONS.length - 1
+            ? 0
+            : incrementedIndex;
+        this.setState({
+            symbolRotationIndex,
+            symbolRotation: CLOCKWISE_SIGN_WRITING_SYMBOL_ROTATIONS[symbolRotationIndex],
+        });
+    }
+
+    rotateCounterClockwise() {
+        const decrementedIndex = this.state.symbolRotationIndex - 1;
+        const symbolRotationIndex = decrementedIndex < 0
+            ? CLOCKWISE_SIGN_WRITING_SYMBOL_ROTATIONS.length - 1
+            : decrementedIndex;
+        this.setState({
+            symbolRotationIndex,
+            symbolRotation: CLOCKWISE_SIGN_WRITING_SYMBOL_ROTATIONS[symbolRotationIndex],
+        });
+    }
+
+    setRightHand() {
+        this.setState({isRightHanded: true});
+    }
+
+    setLefttHand() {
+        this.setState({isRightHanded: false});
+    }
+
+    getPalmTowardsSymbol(hands: Hand[]): string {
+        for(const i in hands) {
+            const hand = hands[i];
+            if (hand.palm_towards) {
+                return hand.symbol;
+            }
+        }
+        return "񆄡";
+    }
+
+    getPalmSidewaysSymbol(hands: Hand[]): string {
+        for(const i in hands) {
+            const hand = hands[i];
+            if (hand.palm_sideways) {
+                return hand.symbol;
+            }
+        }
+        return "񆄱";
+    }
+
+    getPalmAwaySymbol(hands: Hand[]): string {
+        for(const i in hands) {
+            const hand = hands[i];
+            if (hand.palm_away) {
+                return hand.symbol;
+            }
+        }
+        return "񆅁";
+    }
+
+    getPalmButtonSymbols(): {
+        palmTowardsSymbol: string,
+        palmSidewaysSymbol: string,
+        palmAwaySymbol: string,
+    } {
+        const palmFilterHands = this.state.palmFilterHands.filter(hand =>
+            hand.right_handed === this.state.isRightHanded &&
+            hand.symbol_rotation === this.state.symbolRotation
+        );
+        return {
+            palmTowardsSymbol: this.getPalmTowardsSymbol(palmFilterHands),
+            palmSidewaysSymbol: this.getPalmSidewaysSymbol(palmFilterHands),
+            palmAwaySymbol: this.getPalmAwaySymbol(palmFilterHands),
+        };
     }
 
     render() {
+        const{
+            palmTowardsSymbol,
+            palmSidewaysSymbol,
+            palmAwaySymbol
+        } = this.getPalmButtonSymbols();
         return (
             <div className="container-fluid" id="sign-languages">
                 <Select
@@ -86,8 +177,15 @@ export class SignLanguages extends React.Component<Props, State> {
                             name: "Hand",
                             type: ToolbarType.TOGGLE,
                             buttons: [
-                                {text: "Left"},
-                                {text: "Right", isActive: true},
+                                {
+                                    text: "Left",
+                                    handleClick: this.setLefttHand.bind(this),
+                                },
+                                {
+                                    text: "Right",
+                                    isActive: true,
+                                    handleClick: this.setRightHand.bind(this),
+                                },
                             ],
                         },
                         {
@@ -95,16 +193,16 @@ export class SignLanguages extends React.Component<Props, State> {
                             type: ToolbarType.TOGGLE,
                             buttons: [
                                 {
-                                    text: "񆄡",
+                                    text: palmTowardsSymbol,
                                     isActive: true,
                                     classes: [PHONEME_SYMOL_CLASS],
                                 },
                                 {
-                                    text: "񆄱",
+                                    text: palmSidewaysSymbol,
                                     classes: [PHONEME_SYMOL_CLASS],
                                 },
                                 {
-                                    text: "񆅁",
+                                    text: palmAwaySymbol,
                                     classes: [PHONEME_SYMOL_CLASS],
                                 },
                             ],
@@ -112,8 +210,12 @@ export class SignLanguages extends React.Component<Props, State> {
                         {
                             type: ToolbarType.CLICKABLE_BUTTON,
                             buttons: [
-                                {text: "↺"},
-                                {text: "↻"}
+                                {
+                                    text: "↺"
+                                },
+                                {
+                                    text: "↻"
+                                },
                             ],
                         },
                     ]}
